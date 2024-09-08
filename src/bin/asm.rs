cb5addd077d1a39093d54c3feadde643801b1ae5
Debapratim <DebapratimShyam94@gmail.com>
2024-09-08 23:34:15 +0530

    Building out the assembler

--- src/bin/asm.rs
+++ src/bin/asm.rs
@@ -4,6 +4,61 @@ use std::path::Path;
 use std::io;
 use std::io::{BufReader, BufRead, Write};
 
+use rusticvm::{Instruction, OpCode, Register};
+
+fn parse_numeric(s: &str) -> Result<u8, String> {
+    // TODO: handle hex, binary numbers
+    if s.len() == 0 {
+        return Err("string has no length".to_string());
+    }
+    let first = s.chars().nth(0).unwrap();
+    let (num, radix) = match first {
+        '$' => (&s[1..], 16),
+        '%' => (&s[1..], 2),
+        _ => (s, 10)
+    };
+    return u8::from_str_radix(num, radix).map_err(|x| format!("{}", x));
+}
+
+fn parse_register(s: &str) -> Result<Register, String> {
+    return match s {
+        "A" => Ok(Register::A),
+        _ => Err(format!("unknown register: {}", s)),
+    };
+}
+
+fn assert_length(parts: &Vec<&str>, n: usize) -> Result<(), String> {
+    if parts.len() == n {
+        return Ok(());
+    } else {
+        return Err(format!("expected {} got {}", parts.len(), n));
+    }
+}
+
+fn handle_line(parts: Vec<&str>) -> Result<Instruction, String> {
+    let opcode = OpCode::from_str(parts[0]).ok_or(format!("unkown opcode: {}", parts[0]))?;
+
+    match opcode {
+        OpCode::Push => {
+            assert_length(&parts, 2)?;
+            Ok(Instruction::Push(parse_numeric(parts[1])?))
+        },
+        OpCode::AddStack => {
+            assert_length(&parts, 1)?;
+            Ok(Instruction::AddStack)
+        },
+        OpCode::PopRegister => {
+            assert_length(&parts, 2)?;
+            Ok(Instruction::PopRegister(parse_register(parts[1])?))
+        }
+        OpCode::Signal => {
+            assert_length(&parts, 2)?;
+            Ok(Instruction::Signal(parse_numeric(parts[1])?))
+        }
+        _ => Err(format!("un-implemented opcode: {:?}", opcode))
+    }
+}
+
 fn main() -> Result<(), String>{
     // ./asm file.asm
 
@@ -15,13 +70,28 @@ fn main() -> Result<(), String>{
     let input_file_name = &args[1];
     
     let file = File::open(Path::new(input_file_name)).map_err(|err| format!("failed to open file: {}", err))?;
-
     let mut output: Vec<u8> = Vec::new();
-    for line in BufReader::new(file).lines().map(|l| l.unwrap()) {
-        for token in line.split(" ").filter(|x| x.len() > 0) {
-            let b = u8::from_str_radix(token, 16).map_err(|err| format!("failed to parse int: {}", err))?;
-            output.push(b);
+
+    // PUSH 10
+    // PUSH 8
+    // ADDSTACK
+    for line in BufReader::new(file).lines() {
+        let line_inner = line.map_err(|_x| "foo")?;
+        if line_inner.len() == 0 {
+            continue;
         }
+        if line_inner.chars().nth(0).unwrap() == ';' {
+            continue;
+        }
+        let parts: Vec<_> = line_inner.split(" ").filter(|x| x.len() > 0).collect();
+        if parts.len() == 0 {
+            continue;
+        }
+        let instruction = handle_line(parts)?;
+        let raw_instruction: u16 = instruction.encode_u16();
+        // assumption: "">> 8" needs no mask for u16
+        output.push((raw_instruction & 0xff) as u8);
+        output.push((raw_instruction >> 8) as u8);
     }
 
     let mut handle = io::stdout().lock();
--- src/vm.rs
+++ src/vm.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use crate::memory::{LinearMemory, Addressable};
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 #[repr(u8)]
 pub enum Register {
     A, B, C, M, SP, PC, BP, FLAGS,
@@ -23,9 +23,8 @@ impl Register {
     }
 }
 
-#[repr(u8)]
 #[derive(Debug)]
-pub enum Op {
+pub enum Instruction {
     Nop,
     Push(u8),
     PopRegister(Register),
@@ -34,9 +33,71 @@ pub enum Op {
     Signal(u8),
 }
 
-impl Op {
-    pub fn value(&self) -> u8 {
-        unsafe { *<*const _>::from(self).cast::<u8>() }
+impl Instruction {
+
+    fn encode_r1(r: Register) -> u16 {
+        ((r as u16) & 0xf) << 8
+    }
+
+    fn encode_r2(r: Register) -> u16 {
+        ((r as u16) & 0xf) << 12
+    }
+
+    fn encode_num(u: u8) -> u16 {
+        (u as u16) << 8
+    }
+
+    fn encode_rs(r1: Register, r2: Register) -> u16 {
+        Self::encode_r1(r1) | Self::encode_r2(r2)
+    }
+
+     pub fn encode_u16(&self) -> u16 {
+        match self {
+            Self::Nop => OpCode::Nop as u16,
+            Self::Push(x) => OpCode::Push as u16 | Self::encode_num(*x),
+            Self::PopRegister(r) => OpCode::PopRegister as u16 | Self::encode_r1(*r),
+            Self::Signal(s) => OpCode::Signal as u16 | Self::encode_num(*s),
+            Self::AddStack => OpCode::AddStack as u16,
+            Self::AddRegister(r1, r2) => OpCode::AddRegister as u16 | Self::encode_rs(*r1, *r2),
+        }
+    }
+}
+
+#[derive(Debug)]
+#[repr(u8)]
+pub enum OpCode {
+    Nop = 0x0,
+    Push = 0x1,
+    PopRegister = 0x2,
+    Signal = 0x0f, 
+    AddStack = 0x10,
+    AddRegister = 0x11,
+}
+
+impl OpCode {
+
+    pub fn from_str(s: &str) -> Option<Self> {
+        match s {
+            "Nop" => Some(Self::Nop),
+            "Push" => Some(Self::Push),
+            "PopRegister" => Some(Self::PopRegister),
+            "Signal" => Some(Self::Signal),
+            "AddStack" => Some(Self::AddStack),
+            "AddRegister" => Some(Self::AddRegister),
+            _ => None,
+        }
+    }
+
+    pub fn from_u8(b: u8) -> Option<Self> {
+        match b {
+            x if x == Self::Nop as u8 => Some(Self::Nop),
+            x if x == Self::Push as u8=> Some(Self::Push),
+            x if x == Self::PopRegister as u8 => Some(Self::PopRegister),
+            x if x == Self::Signal as u8 => Some(Self::Signal),
+            x if x == Self::AddStack as u8 => Some(Self::AddStack),
+            x if x == Self::AddRegister as u8 => Some(Self::AddRegister),
+            _ => None,
+        }
     }
 }
 
@@ -44,28 +105,36 @@ fn parse_instruction_arg(ins: u16) -> u8 {
     return ((ins & 0xff00) >> 8) as u8;
 }
 
-fn parse_instruction(ins: u16) -> Result<Op, String> {
+fn parse_instruction(ins: u16) -> Result<Instruction, String> {
     let op = (ins & 0xff) as u8;
-    match op {
-        x if x == Op::Nop.value() => Ok(Op::Nop),
-        x if x == Op::Push(0).value() => {
+    match OpCode::from_u8(op).ok_or(format!("unknown op: {:X}", op))? {
+        OpCode::Nop => Ok(Instruction::Nop),
+        OpCode::Push => {
             let arg = parse_instruction_arg(ins);
-            return Ok(Op::Push(arg));
+            return Ok(Instruction::Push(arg));
         },
-        x if x == Op::PopRegister(Register::A).value() => {
+        OpCode::PopRegister => {
             let reg = (ins & 0xf00) >> 8;
-            Register::from_u8(reg as u8)
+            return Register::from_u8(reg as u8)
                 .ok_or(format!("unkown register 0x{:X}", reg))
-                .map(|r| Op::PopRegister(r))
+                .map(|r| Instruction::PopRegister(r));
         },
-        x if x == Op::AddStack.value() => {
-            return Ok(Op::AddStack);
+        OpCode::AddStack => {
+            return Ok(Instruction::AddStack);
         },
-        x if x == Op::Signal(0).value() => {
+        OpCode::Signal => {
             let arg = parse_instruction_arg(ins);
-            return Ok(Op::Signal(arg));
+            return Ok(Instruction::Signal(arg));
         },
-        _ => Err(format!("unknown operator 0x{:x}", op)),
+        OpCode::AddRegister => {
+            let reg1_raw = (ins & 0xf00) >> 8;
+            let reg2_raw = (ins & 0xf000) >> 12;
+            let reg1 = Register::from_u8(reg1_raw as u8)
+                .ok_or(format!("unkown register 0x{:X}", reg1_raw))?;
+            let reg2 = Register::from_u8(reg2_raw as u8)
+                .ok_or(format!("unkown register 0x{:X}", reg2_raw))?;
+            return Ok(Instruction::AddRegister(reg1, reg2));
+        }
     }
 }
 
@@ -117,34 +186,34 @@ impl Machine {
 
     pub fn step(&mut self) -> Result<(), String> {
         let pc = self.registers[Register::PC as usize];
-        let instruction = self.memory.read2(pc).ok_or(format!("PC read failed at 0x{:X}", pc))?;
+        let raw_instruction = self.memory.read2(pc).ok_or(format!("PC read failed at 0x{:X}", pc))?;
         self.registers[Register::PC as usize] = pc + 2;
 
-        let op = parse_instruction(instruction)?;
-        match op {
-            Op::Nop => Ok(()),
-            Op::Push(arg) => {
+        let instruction = parse_instruction(raw_instruction)?;
+        match instruction {
+            Instruction::Nop => Ok(()),
+            Instruction::Push(arg) => {
                 self.push(arg.into())
             },
-            Op::PopRegister(reg) => {
+            Instruction::PopRegister(reg) => {
                 let value = self.pop()?;
                 self.registers[reg as usize] = value;
                 Ok(())
             },
-            Op::AddStack => {
+            Instruction::AddStack => {
                 let a = self.pop()?;
                 let b = self.pop()?;
                 self.push(a + b)
             },
-            Op::AddRegister(r1, r2) => {
+            Instruction::AddRegister(r1, r2) => {
                 self.registers[r1 as usize] += self.registers[r2 as usize];
                 Ok(())
             },
-            Op::Signal(signal) => {
+            Instruction::Signal(signal) => {
                 let sig_fn = self.signal_handlers.get(&signal)
                     .ok_or(format!("unkown signal 0x{:X}", signal))?;
                 sig_fn(self)
-            }
+            },       
         }
     }
 }
