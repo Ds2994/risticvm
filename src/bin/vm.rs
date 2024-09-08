c2bbbc755beffa249cfe25ee2eb6725c619b04e0
Debapratim <DebapratimShyam94@gmail.com>
2024-09-08 11:20:10 +0530

    src/bin/vm.rs - Read program from external bin file

--- src/bin/vm.rs
+++ src/bin/vm.rs
@@ -1,6 +1,30 @@
-use rusticvm::Machine;
+use std::env;
+use std::fs::File;
+use std::path::Path;
+use std::io::{BufReader, Read};
+use rusticvm::{Machine, Register};
 
-pub fn main() -> Result<(), &'static str> {
+pub fn main() -> Result<(), String> {
+    let args: Vec<String> = env::args().collect();
+    
+    if args.len() != 2 {
+        panic!("usage: {} <input>", args[0]);
+    }    
+    let input_file_name = &args[1];
+    
+    let file = File::open(Path::new(input_file_name)).map_err(|err| format!("failed to open file: {}", err))?;
+
+    let mut reader = BufReader::new(file);
+    let mut program: Vec<u8> = Vec::new();
+    reader.read_to_end(&mut program).map_err(|err| format!("failed to read file: {}", err))?;
+
     let mut vm = Machine::new();
-    vm.step()
+    vm.memory.load_from_vec(&program, 0);
+    vm.step()?;
+    vm.step()?;
+    vm.step()?;
+    vm.step()?;
+
+    println!("A = {}", vm.get_register(Register::A));
+    Ok(())
 }
